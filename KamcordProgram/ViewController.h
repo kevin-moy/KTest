28996e38050b678f36f83a1fad9a81c801c0f4ff
Kevin <kevintmoy@gmail.com>
2015-06-22 16:52:09 -0700

    First commit

---
+++ KamcordProgram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KamcordProgram
+//
+//  Created by Kevin Moy on 6/22/15.
+//  Copyright (c) 2015 Kevin Moy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KamcordProgram/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KamcordProgram
+//
+//  Created by Kevin Moy on 6/22/15.
+//  Copyright (c) 2015 Kevin Moy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
